84e240723b52c46cdaadc2e736b7acdb8531d8a4
andrew leong <andrew.leong@jewelpaymentech.com>
2019-07-10 23:04:06 +0800

    Setup react router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import App from './App';
+import characterDetail from './components/CharacterDetail';
 
 
 ReactDOM.render(
     <Provider store={configureStore()}>
-        <App />,
+        <BrowserRouter>
+            <div>
+                <Route path="/" component={App} />
+                <Route path="/character/:id" component={characterDetail} />
+            </div>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
