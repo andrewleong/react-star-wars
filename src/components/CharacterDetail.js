1c1081bf5a0d01a21fda2d87d83b2ca2b9bdcb9f
andrew leong <andrew.leong@jewelpaymentech.com>
2019-07-11 22:53:07 +0800

    Add information to render

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -16,9 +16,46 @@ class CharacterDetail extends React.Component {
     }
 
     render() {
+        const {
+            character,
+            films,
+            homeWorld,
+            species
+        } = this.props;
+        console.log("props", this.props)
         return (
-            <div>
-
+            <div className="character-detail-container">
+                <h2>Character Details</h2>
+                <div className="character-content">
+                    <h3>Name: {character.name}</h3>
+                    <p>{character.gender}</p>
+                    <p>{character.hair_color}</p>
+                    <p>{character.height}</p>
+                    <p>{character.mass}</p>
+                    <p>{character.skin_color}</p>
+                    <p>{character.birth_year}</p>
+                    <p>{character.eye_color}</p>
+                </div>
+                <div className="character-content">
+                    <h3>Home World</h3>
+                    <p>{homeWorld.name}</p>
+                </div>
+                <div className="character-content">
+                    <h3>Films</h3>
+                    {
+                        films && films.map((film, index) => {
+                            return <p key={index}>{film.title}</p>
+                        })
+                    }
+                </div>
+                <div className="character-content">
+                    <h3>Species</h3>
+                    {
+                        species && species.map((s, index) => {
+                            return <p key={index}>{s.name}</p>
+                        })
+                    }
+                </div>
             </div>
         );
     }
@@ -46,7 +83,6 @@ class CharacterDetail extends React.Component {
 
 const mapStateToProps = state => {
     const { characters, character, films, species, homeWorld } = state;
-    console.log("state", state)
     return {
         characters,
         character,
