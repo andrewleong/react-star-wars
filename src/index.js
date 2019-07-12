83584034207a2bda7714a0a86f9ff64414fcdcc2
andrew leong <andrew.leong@jewelpaymentech.com>
2019-07-13 03:12:01 +0800

    Remove unused css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
